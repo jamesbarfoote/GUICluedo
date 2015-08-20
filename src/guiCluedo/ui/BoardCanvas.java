bfcba267b72a8bf3c7781b1f84505fcea2689d2a
CombuskenKid <Cameronporter95@gmail.com>
2015-08-20 16:16:35 +1200

    Continued work on drawing the board

--- src/guiCluedo/ui/BoardCanvas.java
+++ src/guiCluedo/ui/BoardCanvas.java
@@ -4,7 +4,6 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -14,7 +13,7 @@ public class BoardCanvas extends Canvas{
 	
 	private static final long serialVersionUID = 1L;
 	Board gameBoard;
-	private String[][] board;
+	private String[][] board = new String[21][23];
 	
 	public BoardCanvas(Board b)
 	{
@@ -22,15 +21,22 @@ public class BoardCanvas extends Canvas{
 		setBackground (Color.BLUE);
         setSize(300, 300);
         readFile();
+        for(int i = 0; i < board.length; i++){
+        	for(int j = 0; j < board[i].length; j++){
+        		System.out.printf(board[i][j]);
+        	}
+        	System.out.println();
+        }
         repaint();
 	}
 	
 	public void paint(Graphics g) {
 		Graphics2D g2;
         g2 = (Graphics2D) g;
-        g2.drawString ("It is a custom canvas area", 70, 70);
-        g.setColor(Color.BLACK);
-        g2.fillRect(50, 50, 50, 50);
+        for(int i = 0; i < board.length; i++){
+        	drawRow(i, g2);
+        }
+        
 	}
 
 	@SuppressWarnings("resource")
@@ -71,16 +77,41 @@ public class BoardCanvas extends Canvas{
 	/**
 	 * Draws a row of the board. There are 21 rows 
 	 * equal to the height of the board.
-	 * @param - The current row to draw
+	 * @param row - The current row to draw
+	 * @param g - The graphics object
 	 */
-	private void drawRow(int row, Graphics g){
-		int roomCount = 0;
+	private void drawRow(int row, Graphics2D g){
+		int roomFrom = 0;
+		int roomTo = 0;
+		int centreFrom = 0;
+		int centreTo = 0;
+		String location = "Hallway";
 		for(int i = 0; i < board[row].length; i++){
-			if(board[row][i].equals("R")){
-				roomCount++;
+			if(board[row][i].equals("R") && location.equals("Hallway")){
+				roomFrom = roomTo;
+				roomTo++;
+				location = "Rooom";
 			}
-			else if(roomCount > 0){
-				
+			else if(board[row][i].equals("R")){
+				roomTo++;
+			}
+			else if(board[row][i].equals("C") && location.equals("Hallway")){
+				centreFrom = centreTo;
+				centreTo++;
+				location = "Centre";
+			}
+			else if(board[row][i].equals("C")){
+				centreTo++;
+			}
+			else if(roomTo - roomFrom > 0){
+				g.setColor(Color.GRAY);
+				g.fillRect(roomFrom*10, row*10, (roomTo - roomFrom)*10, row*10);
+				location = "Hallway";
+			}
+			else if(centreTo - centreFrom > 0){
+				g.setColor(Color.RED);
+				g.fillRect(centreFrom*10, row*10, (centreTo - centreFrom)*10, row*10);
+				location = "Hallway";
 			}
 		}
 	}
